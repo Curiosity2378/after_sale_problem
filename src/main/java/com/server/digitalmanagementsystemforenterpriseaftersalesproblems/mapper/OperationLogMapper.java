8cd3703a8661c962c15de3fc8986adc9d3ae2556
Curiosity2378 <945911566@qq.com>
2023-09-01 22:42:36 +0800

    commit end!

--- src/main/java/com/server/digitalmanagementsystemforenterpriseaftersalesproblems/mapper/OperationLogMapper.java
+++
@@ -1,7 +0,0 @@
-package com.server.digitalmanagementsystemforenterpriseaftersalesproblems.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.server.digitalmanagementsystemforenterpriseaftersalesproblems.entity.OperationLog;
-
-public interface OperationLogMapper extends BaseMapper<OperationLog> {
-}
--- src/main/java/com/server/digitalmanagementsystemforenterpriseaftersalesproblems/service/OperationLogService.java
+++
@@ -1,7 +0,0 @@
-package com.server.digitalmanagementsystemforenterpriseaftersalesproblems.service;
-
-import com.server.digitalmanagementsystemforenterpriseaftersalesproblems.entity.OperationLog;
-
-public interface OperationLogService {
-    void insert(OperationLog operationLog);
-}
